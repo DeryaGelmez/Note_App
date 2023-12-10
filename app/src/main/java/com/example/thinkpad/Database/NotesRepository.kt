1168302a2b53fec1e4c4d51feb80003580b659ce
Derya Gelmez <derya.glmz02@gmail.com>
2023-12-11 01:44:38 +0300

    Son işlemler

--- app/src/main/java/com/example/thinkpad/Database/NotesRepository.kt
+++ app/src/main/java/com/example/thinkpad/Database/NotesRepository.kt
@@ -5,7 +5,7 @@ import com.example.thinkpad.Models.Note
 
 class NotesRepository(private val noteDao: NoteDao) {
 
-    val allNotes:LiveData<List<Note>>= noteDao.getAllNotes()
+    val allNotes:LiveData<List<Note>> = noteDao.getAllNotes()
 
 suspend fun insert(note:Note){
     noteDao.insert(note)
