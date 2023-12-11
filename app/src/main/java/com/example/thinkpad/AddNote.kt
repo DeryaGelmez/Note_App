0379a0b06e40fd34bf30cafd1e58d9147d7f0fde
Derya Gelmez <derya.glmz02@gmail.com>
2023-12-11 22:09:53 +0300

    Test

--- app/src/main/java/com/example/thinkpad/AddNote.kt
+++ app/src/main/java/com/example/thinkpad/AddNote.kt
@@ -1,21 +1,18 @@
 package com.example.thinkpad
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Note
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.thinkpad.Models.Note
 import com.example.thinkpad.databinding.ActivityAddNoteBinding
-import com.example.thinkpad.databinding.ActivityMainBinding
-import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.Date
 
 class AddNote : AppCompatActivity() {
 
 private lateinit var binding: ActivityAddNoteBinding
-private lateinit var note:Note
+private lateinit var note: Note
 private lateinit var old_note:Note
 var isUpdate=false
 
@@ -26,7 +23,7 @@ var isUpdate=false
         setContentView(binding.root)
 
 try{
-    old_note=intent.getSerializableExtra("current_note") as Note
+    old_note= intent.getSerializableExtra("current_note") as Note
     binding.etTitle.setText(old_note.title)
     binding.etNote.setText(old_note.note)
     isUpdate=true
@@ -42,7 +39,7 @@ val title=binding.etTitle.text.toString()
     if (title.isNotEmpty() || note_desc.isNotEmpty()){
         val formatter=SimpleDateFormat("EEE, d MMM yyyy HH:mm a")
         if(isUpdate){
-            note=Note(
+            note = Note(
                 old_note.id,title,note_desc,formatter.format(Date())
             )
             }
@@ -53,7 +50,7 @@ val title=binding.etTitle.text.toString()
         }
         val intent= Intent()
         intent.putExtra("note",note)
-        setResult(Activity.RESULT_OK,intent)
+        setResult(RESULT_OK,intent)
         finish()
 
 
